c0864c856c57b503231e5a6070a7b61ae5a7b940
MX-Steve <2513556525@qq.com>
2017-12-28 16:08:06 +0800

    first commit

---
+++ refs/js/page.js
@@ -0,0 +1,155 @@
+﻿
+	function page(opt)
+	{
+		var url = 'javascript:void(0);';
+		var curclass = 'curpage';//当前页样式
+		var preclass = 'prePage';//上下一页样式
+		var firstclass = 'firstPage';//首尾页样式
+		var allclass = "numPage";
+
+		if(!opt){return false;}
+
+		var obj = document.getElementById(opt.id);
+
+		var nowNum = opt.nowNum || 1;
+		var allNum = opt.allNum || 1;
+
+		if(allNum < 2)
+		{
+			return;
+		}
+
+
+		/*if(nowNum >= 4 && allNum >=6)
+		{
+			var sy = document.createElement("a");
+			sy.href = url;
+			sy.innerHTML = "首页";
+			sy.className = firstclass;
+			sy.setAttribute("cur",1);
+			obj.appendChild(sy);
+		}*/
+
+		if(nowNum >=0)
+		{
+			var syy = document.createElement("a");
+			syy.href = url;
+			syy.innerHTML = "<";
+			syy.className = preclass;
+			syy.setAttribute("cur",(nowNum - 1));
+			obj.appendChild(syy);
+		}
+
+		if(allNum <= 5 )
+		{
+			for (var i = 1; i <= allNum; i++) 
+			{
+				var OA = document.createElement('a');
+				OA.href =url;
+					
+					if(nowNum == i)
+					{
+						OA.innerHTML = i;
+						OA.className = curclass;
+						OA.setAttribute("cur",i);
+					}
+					else
+					{
+						OA.innerHTML = i;
+						OA.setAttribute("cur",i);
+					}
+					obj.appendChild(OA);
+			};
+		}
+		else
+		{
+			for (var i = 1; i <= 5; i++) 
+			{
+				var OA = document.createElement('a');	
+				if(nowNum == 1 || nowNum == 2)
+				{
+					OA.href = url;
+					OA.setAttribute("cur",i);	
+					if(nowNum == i)
+					{
+						OA.innerHTML = i;
+						OA.className = curclass;
+						OA.setAttribute("cur",i);
+					}
+					else
+					{
+						OA.innerHTML = i;
+						OA.setAttribute("cur",i);
+					}
+				}
+
+				else if((allNum - nowNum) == 0 || (allNum - nowNum) == 1 )
+				{
+					OA.href = url;
+					OA.setAttribute("cur",(allNum - 5 + i));
+
+					if((allNum - nowNum) == 0  && i==5)
+					{
+						OA.innerHTML = (allNum - 5 + i);
+						OA.className = curclass;
+					}
+					else if((allNum - nowNum) == 1 && i == 4)
+					{
+						OA.innerHTML = (allNum - 5 + i);
+						OA.className = curclass;
+					}
+					else
+					{
+						OA.innerHTML = (allNum - 5 + i);
+					}
+				}
+
+				else
+				{
+					
+					OA.href = url;
+					OA.setAttribute("cur",(nowNum - 3 + i));
+					if(i == 3)
+					{
+						OA.innerHTML = (nowNum - 3 + i);
+						OA.className = curclass;
+					}	
+					else
+					{
+						OA.innerHTML = (nowNum - 3 + i);
+
+					}
+				}
+				
+				obj.appendChild(OA);	
+			}
+			var toal = document.createElement("a");
+			toal.innerHTML ="...";
+			toal.className = allclass;
+			obj.appendChild(toal);
+			var  totalNum= document.createElement("a");
+			totalNum.innerHTML = allNum;
+			totalNum.className = allclass;
+			obj.appendChild(totalNum);
+		}
+
+		if((allNum - nowNum >= 1))
+		{
+			var xyy = document.createElement("a");
+			xyy.href = url;
+			xyy.innerHTML =">";
+			xyy.className = preclass;
+			xyy.setAttribute("cur",(nowNum + 1));
+			obj.appendChild(xyy);
+		}
+
+		/*if((allNum - nowNum)>= 3 && allNum >=6)
+		{
+			var wy = document.createElement("a");
+			wy.href = url;
+			wy.className = firstclass;
+			wy.setAttribute("cur",allNum);
+			wy.innerHTML = "尾页";
+			obj.appendChild(wy);
+		}*/
+	}
---
+++ refs/js/time_until.js
@@ -0,0 +1,23 @@
+﻿    function theTime(){
+    var weekDayLabels = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour=now.getHours();   //获取小时
+    var minues;  // 获取分钟
+    if(now.getMinutes() < 10){
+        minues = "0" + now.getMinutes();
+    }else{
+        minues = now.getMinutes();
+    }
+    var second;  //  获取秒
+    if(now.getSeconds() < 9){
+        var num = now.getSeconds()+1;
+        second = "0" + num;
+    }else{
+        second = now.getSeconds()+1;
+    }
+    document.getElementById("now_date").innerHTML ="今天是" + year + "年" + month + "月" + day + "日 "+"&nbsp;&nbsp;"+hour+":" + minues +":"+ second +"&nbsp;&nbsp;&nbsp;&nbsp;" + weekDayLabels[now.getDay()]
+}
+setInterval("theTime()",1000);
